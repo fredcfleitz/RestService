95ea592e5b30159db5f171b6b56083bd238dde7d
squareballer <squarjn@gmail.com>
2020-05-22 20:39:36 -0400

    first commit

---
+++ src/main/java/com/example/restservice/RestService.java
@@ -0,0 +1,13 @@
+package com.example.restservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestService {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestService.class, args);
+	}
+
+}
